8da7a2b8783fc3e4eeec9fa0ac0b8c77634b4456
kkbp27 <kotchakon391@gmail.com>
2024-02-05 11:26:46 +0700

    Quiz app

---
+++ quiz-app/src/component/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useContext} from "react";
+import {DataContext} from "../App";
+const Menu = () =>{
+    const {setAppState} = useContext(DataContext)
+    return(
+        <div className="menu">
+            <h1>Menu Component</h1>
+            <button onClick={()=>setAppState("quiz")}>เริ่มทำแบบสอบถาม</button>
+        </div>
+    )
+}
+export default Menu;
